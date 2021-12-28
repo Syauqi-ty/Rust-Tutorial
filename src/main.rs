ded1f58f455ed1a6f853f6730905367ed6414aea
Syauqi-ty <55091752+Syauqi-ty@users.noreply.github.com>
2021-12-28 14:44:36 +0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let y = vec![1,2,3];
+    for (index,elem) in y.iter().enumerate() {
+       println!("{} {}",index,elem);
+    }
+}
---
+++ target/debug/deps/syoki-ceefa4733c1a18fe.d
@@ -0,0 +1,5 @@
+C:\Users\Syauqittuqa\Desktop\Kerjaan\rust iseng\syoki\target\debug\deps\syoki-ceefa4733c1a18fe.exe: src\main.rs
+
+C:\Users\Syauqittuqa\Desktop\Kerjaan\rust iseng\syoki\target\debug\deps\syoki-ceefa4733c1a18fe.d: src\main.rs
+
+src\main.rs:
